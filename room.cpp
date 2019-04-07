ceffe4e3bf495fafc2b85d4ba802033914c7dc1a
kimsekai94 <kristingtong#gmail.com>
2019-04-07 17:32:52 +0700

    newwwwwwwwwwwww

--- room.cpp
+++ room.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+void greentext(){SetConsoleTextAttribute(hConsole, 2);  };
+void whitetext(){SetConsoleTextAttribute(hConsole, 15);  };
+void redtext (){SetConsoleTextAttribute(hConsole,  4);  };
+void bluetext (){SetConsoleTextAttribute(hConsole, 11);  };
+void yellowtext (){SetConsoleTextAttribute(hConsole, 14);  };
+void darktext (){SetConsoleTextAttribute(hConsole, 8);  };
 void room(int , int );
 //void detailroom(int);
 
@@ -10,34 +18,33 @@ int main(){
     cin >> a;
     cout << " C : ";
     cin >> c2;
-    cout << "   ------------------------------------  \n";
+    cout << "------------------------------------  \n";
     room(a,c2);
 
 }
 
 
 void room(int b,int s){
-	cout << "-----------------------\n";
+
+
+
+
+
 	int j = 0;
     if(b<=2 && s<=2) j = 2;
     if(b>2 && b<=4 && s<=2) j = 3;
-    cout << j <<"\n";
-    cout << "-----------------------\n";
     int i = 0;
-    while( i <= j )
-    {
-        if (j != 3) {
-            if (i == 1) {
-            cout << i  << ".";
+    redtext ();
+    if (j != 3) cout << "***** Appropriate rooms. *****\n";
+    whitetext();
+            cout << "1.";
             cout << " Standard Studio \n";
             cout << "   - Price per night : 600 Baht\n";
             cout << "   - 2 Single bed or 1 Queen Sized  \n";
             cout << "   - Non-smoking  \n";
             cout << "   - Private Bathroom  \n";
             cout << "   ------------------------------------  \n";
-            }
-        if (i == 2) {
-            cout << i  << ".";
+            cout << "2.";
             cout << " Deluxe Studio \n"; 
             cout << "   - Price per night : 900 Baht\n";
             cout << "   - 1 king bed  \n";
@@ -46,10 +53,15 @@ void room(int b,int s){
             cout << "   - Non-smoking  \n";
             cout << "   - Private Bathroom  \n";
             cout << "   ------------------------------------  \n";
-            }
-        }else if(i==1){
-        	
-        		cout << i  << ".";
+        
+     if (j == 3){
+         redtext ();
+         cout << "***** Appropriate rooms. *****\n"; 
+         whitetext();
+         cout << "1.";
+     }else{
+         cout << "3.";
+     }
 				cout << " Standard Family \n";
                 cout << "   - Price per night : 1200 Baht\n";
                 cout << "   - 1 king bed and 1 bunk bed   \n";
@@ -58,10 +70,4 @@ void room(int b,int s){
                 cout << "   - Non-smoking  \n";
                 cout << "   - Private Bathroom  \n";
                 cout << "   ------------------------------------  \n";
-        		
-			
-            
-            }
-            i ++;
-			}	
 			}
